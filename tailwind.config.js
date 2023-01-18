8ab8b00ead75afb04bbdd50ba1499465154c63ff
ARTWORKHEX <112499761+artworkhex@users.noreply.github.com>
2023-01-18 21:02:09 +0700

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,32 @@
+module.exports = {
+  content: [
+    './index.html',
+    './*/.html',
+  ],
+
+
+  theme: {
+    screens: {
+      xm: "220px",
+      xm1: "270px",
+      sm: "320px",
+      sm1: "400px",
+      md: "588px",
+      md1: "620px",
+      lg: "776px",
+      xl: "1240px",
+    },
+    letterSpacing: {
+      tightest: '-.075em',
+      tighter: '-.05em',
+      tight: '-.025em',
+      normal: '0',
+      wide: '.025em',
+      wider: '.05em',
+      widest: '.1em',
+      widest: '.55em',
+    },
+    extend: {},
+  },
+  plugins: [],
+}
